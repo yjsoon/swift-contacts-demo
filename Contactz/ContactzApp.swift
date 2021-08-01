5175c40ff9ec810ab01b262e2f2cdce8773b7ff4
YJ Soon <yjsoon@tinkertanker.com>
2021-08-01 14:44:44 +0800

    Initial Commit

---
+++ Contactz/ContactzApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactzApp.swift
+//  Contactz
+//
+//  Created by YJ Soon on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct ContactzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
