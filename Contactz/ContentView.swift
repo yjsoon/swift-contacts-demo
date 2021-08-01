2aa768d219806b3da48efe049990d6651c179bfe
YJ Soon <yjsoon@tinkertanker.com>
2021-08-01 15:24:52 +0800

    Added some friends in a Navigation View

--- Contactz/ContentView.swift
+++ Contactz/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Text("Alice")
+                Text("Bob")
+                Text("Charles")
+            }
+            .navigationTitle("Contacts")
+        }
+        
     }
 }
 
