ae339a0df5262bf262d6428f72cc1f632ceb4c62
YJ Soon <yjsoon@tinkertanker.com>
2021-08-08 15:53:06 +0800

    Added more stuff to contact detail view

--- Contactz/ContactDetailView.swift
+++ Contactz/ContactDetailView.swift
@@ -12,14 +12,31 @@ struct ContactDetailView: View {
     var contact: Contact
     
     var body: some View {
-        Text(contact.name)
-            .font(.largeTitle)
+        VStack {
+            Image(contact.name)
+                .resizable()
+                .frame(width: 200.0, height: 200.0)
+                .scaledToFit()
+                .mask(Circle())
+                .overlay(Circle()
+                            .stroke(lineWidth: 8)
+                            .foregroundColor(.white))
+                .shadow(radius: 10)
+                .padding(.top, 30.0)
+            Text(contact.name)
+                .font(.largeTitle)
+            Text(contact.company)
+                .font(.headline)
+                .foregroundColor(.gray)
+                .padding(5.0)
+            Spacer()
+        }
             
     }
 }
 
 struct ContactDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        ContactDetailView(contact: Contact(name: "Rick", company: "YouTube"))
+        ContactDetailView(contact: Contact(name: "Alice", company: "YouTube"))
     }
 }
