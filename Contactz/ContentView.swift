c7152a740b4ae004f645265d89c4f2c130003fff
YJ Soon <yjsoon@tinkertanker.com>
2021-08-22 15:45:56 +0800

    Now you can delete friends yay

--- Contactz/ContentView.swift
+++ Contactz/ContentView.swift
@@ -12,25 +12,31 @@ struct ContentView: View {
     @State var contacts = [
         Contact(name: "Alice", company: "Apple", rating: 5, background: "Alice"),
         Contact(name: "Bob", company: "Box"),
-        Contact(name: "Charles", company: "Coconuts")
+        Contact(name: "Charles", company: "Coconuts"),
+        Contact(name: "Blah blah", company: "Donkeys")
     ]
     
     var body: some View {
         NavigationView {
-            List(contacts) { contact in
-                let index = contacts.firstIndex {
-                    $0.id == contact.id
-                }!
-                
-                NavigationLink(destination: ContactDetailView(contact: $contacts[index])) {
-                    VStack(alignment: .leading) {
-                        Text(contact.name)
-                            .font(.headline)
-                        Text(contact.company)
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
+            List {
+                ForEach(contacts) { contact in
+                    let index = contacts.firstIndex {
+                        $0.id == contact.id
+                    }!
+                    
+                    NavigationLink(destination: ContactDetailView(contact: $contacts[index])) {
+                        VStack(alignment: .leading) {
+                            Text(contact.name)
+                                .font(.headline)
+                            Text(contact.company)
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }
                     }
                 }
+                .onDelete(perform: { indexSet in
+                    contacts.remove(atOffsets: indexSet)
+                })
             }
             .navigationTitle("Contacts")
         }
